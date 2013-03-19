dd2f15d37d090bc380429a3dc28b355866490504
Perjan Duro <perjan.other@me.com>
2013-03-19 19:30:18 +0100

    Updated front end for the new auth system
    Added simple_form
    Updated several forms to bootstrap/simple_form
    Added Log in/ Log out register
    Added a collection select to status form

--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,4 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  attr_accessible :content, :user_id
+  belongs_to :user  #assumes that the table status has a field user_id
 end
