5196678ad403950345d0774599a8095183faca9b
Perjan Duro <perjan.other@me.com>
2013-03-19 21:45:55 +0100

    added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def flash_class(type)
+		case type
+		when :alert then "alert-error"
+		when :notice then "alert-success"	
+		else ""
+		end
+	end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,42 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "a user should enter their first name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?
+  end
+
+  test "a user should enter their last name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?
+  end
+
+  test "a user should enter their profile name" do
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a unique profile name" do
+    user = User.new
+
+    user.profile_name = users(:perjan).profile_name
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+  end
+
+  test "a user should have a profile name without spaces" do
+  	user = User.new
+
+  	user.profile_name = "my ori sdsd"
+
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?
+
+  	assert user.errors[:profile_name].include?("Must be formated correctly.")
+
+  end
 end
